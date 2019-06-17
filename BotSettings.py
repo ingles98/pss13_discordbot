f624c57f36bca9af530e22d783672dab8f374b7d
r3valkyrie <valkyrie@r3valkyrie.com>
2019-06-17 15:43:09 -0400

    format code a bit, rename some functions to be more descriptive.

--- BotSettings.py
+++ BotSettings.py
@@ -4,8 +4,15 @@ from InternalClasses.DatabaseDAO import DatabaseDAO
 from InternalClasses.BotActions import BotActions
 from discord.ext.commands import Bot
 
-queue_command_arguments_sep = "[[sep]]" # The seperator between arguments inside a string because spaces could be insufficient. Use the same seperator as in the game's code (code/modules/discord_persistence/_hook.dm)
-main_loop_timer = 2 # Seconds
+"""
+The seperator between arguments inside a string because spaces could be
+insufficient. Use the same seperator as in the game's code:
+
+(code/modules/discord_persistence/_hook.dm)
+"""
+
+queue_command_arguments_sep = "[[sep]]"
+main_loop_timer = 2  # Seconds
 command_prefix = "!"
 token = 'NTg5ODM0NzkxNzU1NzEwNTA2.XQZe-A.h5JV5bB5UbqIxCSiwXeE0eX3uok'
 db_path = ""
@@ -13,34 +20,35 @@ queueTable = ""
 usersTable = ""
 serverId = 0
 generalChannelId = 0
-DB:DatabaseDAO = None
-bot_actions:BotActions = None
-bot_ref:Bot = None
-bot_general_channel_ref:discord.TextChannel = None
+
+DB: DatabaseDAO = None
+bot_actions: BotActions = None
+bot_ref: Bot = None
+bot_general_channel_ref: discord.TextChannel = None
 
 
 with open('config.json') as json_file:
-	data = json.load(json_file)
-	token = data["token"]
-	db_path = data["db"]
-	queueTable = data["queueTable"]
-	usersTable = data["usersTable"]
-	serverId = int(data["serverId"])
-	generalChannelId = int(data["generalChannelId"])
+    data = json.load(json_file)
+    token = data["token"]
+    db_path = data["db"]
+    queueTable = data["queueTable"]
+    usersTable = data["usersTable"]
+    serverId = int(data["serverId"])
+    generalChannelId = int(data["generalChannelId"])
 
-def Setup(bot:Bot):
-	# Save reference to the Bot
-	global bot_ref
-	bot_ref = bot
 
-	# Setup database DAO
-	global DB
-	DB = DatabaseDAO(db_path, usersTable, queueTable)
+def setup_bot(bot: Bot):
+    # Save reference to the Bot
+    global bot_ref
+    bot_ref = bot
 
-	# Setup bot actions class
-	global bot_actions
-	bot_actions = BotActions()
+    # Setup database DAO
+    global DB
+    DB = DatabaseDAO(db_path, usersTable, queueTable)
 
-	# Setup general channel discord object reference
-	global bot_general_channel_ref
-	bot_general_channel_ref = bot.get_channel(generalChannelId)
+    # Setup bot actions class
+    global bot_actions
+    bot_actions = BotActions()
+
+    # Setup general channel discord object reference
+    global bot_general_channel_ref
