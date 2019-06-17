b020fe8fd7b7f593964e62f4f1c5af4cfb9b7419
r3valkyrie <valkyrie@r3valkyrie.com>
2019-06-17 15:43:09 -0400

    format code a bit, rename some functions to be more descriptive.

--- Main.py
+++ Main.py
@@ -1,37 +1,38 @@
-import time
 import asyncio
-import discord
 import traceback
 from discord.ext import commands
 import BotSettings
-from threading import Thread
 
 bot = commands.Bot(command_prefix=BotSettings.command_prefix)
 
 extensions = [
-	'cogs.debug'
+    'cogs.debug'
 ]
 
 for extension in extensions:
-	try:
-		bot.load_extension(extension)
-	except Exception as e:
-		print(e)
-		traceback.print_exc()
+    try:
+        bot.load_extension(extension)
 
-async def MainLoop():
-	while True:
-		messages = BotSettings.DB.get_messages()
-		await BotSettings.bot_actions.process_messages(messages)
-		await asyncio.sleep(BotSettngs.main_loop_timer)
+    except Exception as e:
+        print(e)
+        traceback.print_exc()
+
+
+async def main_loop():
+    while True:
+        messages = BotSettings.DB.get_messages()
+        await BotSettings.bot_actions.process_messages(messages)
+        await asyncio.sleep(BotSettings.main_loop_timer)
+
 
 @bot.event
 async def on_ready():
-	print('Logged in as')
-	print(bot.user.name)
-	print(bot.user.id)
-	print('------')
-	BotSettings.Setup(bot)
-	bot.loop.create_task(MainLoop())
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+    BotSettings.Setup(bot)
+    bot.loop.create_task(main_loop())
 
 bot.run(BotSettings.token, bot=True, reconnect=True)
--- cogs/debug.py
+++ cogs/debug.py
@@ -2,20 +2,25 @@ import BotSettings
 import discord
 from discord.ext import commands
 
+
 class DebugCommands(commands.Cog, name="Debug Commands"):
-	def __init__(self, bot):
-		self.bot = bot
-		self._last_member = None
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
 
-	@commands.command()
-	async def hello(self, ctx, *, member: discord.Member = None):
-		"""Says hello"""
-		member = member or ctx.author
-		if self._last_member is None or self._last_member.id != member.id:
-			await ctx.send('Hello {0.name}~'.format(member))
-		else:
-			await ctx.send('Hello {0.name}... This feels familiar.'.format(member))
-		self._last_member = member
+    @commands.command()
+    async def hello(self, ctx, *, member: discord.Member = None):
+        """Says hello"""
+        member = member or ctx.author
+        if self._last_member is None or self._last_member.id != member.id:
+            await ctx.send('Hello {0.name}~'.format(member))
 
-def setup(bot):
-	bot.add_cog(DebugCommands(bot))
+        else:
+            await ctx.send(
+                    'Hello {0.name}... This feels familiar.'.format(member)
+            )
+            self._last_member = member
+
+    @commands.command()
+    def load_settings(bot):
+        bot.add_cog(DebugCommands(bot))
